f042fb8a9466fac50a96760bca39ff55126b87f1
Pedro Castaneda <petecast76@gmail.com>
2024-12-11 13:06:49 -0800

    feat: added filter options in nav

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const buildPokemonElements = async (getAllPokemonData) => {
         const { name, type, image, id } = await getPokemonDetails(pokemon.url);
 
         const pokemonCard = document.createElement('div');
-        pokemonCard.className = 'flex flex-col items-center justify-center p-2 border border-gray-300 rounded-lg shadow-lg';
+        pokemonCard.className = 'flex flex-col items-center justify-center p-2 border border-orangeWeb rounded-lg shadow-lg shadow-orangeWeb bg-oxfordBlue text-platinum';
         pokemonCard.innerHTML = `
             <p class='text-lg font-semibold self-end'>${id}</p>
             <a href='/src/pages/pokemon-details.html?id=${id}' class='hover:-translate-y-1 hover:scale-105 transition-all ease-in'>
             <img src=${image} alt=${name} class="w-[10rem] h-auto" />
             </a>
-            <p key=${id} class='text-lg font-semibold'>${name}</p>
-            <p>${type}</p>
+            <p key=${id} class='text-lg font-semibold capitalize'>${name}</p>
+            <p class='capitalize italic'>${type}</p>
         `;
 
         pokemonGallery.appendChild(pokemonCard);
@@ -26,8 +26,9 @@ const buildPokemonElements = async (getAllPokemonData) => {
 }
 
 async function getAllPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=30&offset=0');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1302');
     const data = await response.json();
+    console.log(data);
     return data.results;
 }
 
--- src/js/pokemon-details.js
+++ src/js/pokemon-details.js
@@ -3,12 +3,11 @@ const pokemonDetailsContainer = document.getElementById('pokemon-details');
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const pokemonId = urlParams.get('id');
-console.log(pokemonId);
 
 const getPokemonDetailsById = async (id) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon = await response.json();
-    console.log(pokemon);
+
     return {
         name: pokemon.name,
         id: pokemon.id,
@@ -26,14 +25,14 @@ const renderPokemonDetails = async () => {
     const pokemon = await getPokemonDetailsById(pokemonId);
 
     const pokemonDetails = document.createElement('div');
-    pokemonDetails.className = 'flex flex-col items-center justify-center p-2 border border-gray-300 rounded-lg shadow-lg';
+    pokemonDetails.className = 'flex flex-col gap-4 items-center justify-center p-6 border border-orangeWeb rounded-lg bg-oxfordBlue text-platinum shadow-lg shadow-orangeWeb w-[80%]';
     pokemonDetails.innerHTML = `
         <p class='text-lg font-semibold self-end'>${pokemon.id}</p>
-        <img src=${pokemon.image} alt=${pokemon.name} class="w-[10rem] h-auto" />
-        <p key=${pokemon.id} class='text-lg font-semibold'>${pokemon.name}</p>
-        <p>Abilities: ${pokemon.abilities.join(', ')}</p>
-        <p>Moves: ${pokemon.moves.join(', ')}</p>
-        <p>Stats: ${pokemon.stats.map(stat => `${stat.name}: ${stat.value}`).join(', ')}</p>
+        <img src=${pokemon.image} alt=${pokemon.name} class="w-[30rem] h-auto" />
+        <p key=${pokemon.id} class='text-4xl font-semibold capitalize'>${pokemon.name}</p>
+        <p class='capitalize self-start'>Abilities: ${pokemon.abilities.join(', ')}</p>
+        <p class='capitalize self-start'>Moves: ${pokemon.moves.join(', ')}</p>
+        <p class='capitalize self-start'>Stats: ${pokemon.stats.map(stat => `${stat.name}: ${stat.value}`).join(', ')}</p>
     `;
 
     pokemonDetailsContainer.appendChild(pokemonDetails);
