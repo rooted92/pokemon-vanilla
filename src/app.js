c9f7d9d9b4d55beb7ba680f5a3fc9572f5ea36bd
Pedro Castaneda <petecast76@gmail.com>
2024-12-13 15:14:02 -0800

    feat: code clean up, no use for search button and error msg elment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const pokemonGallery = document.getElementById('pokemon-gallery');
 const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-btn');
-const error = document.getElementById('error-msg');
 
 let allPokemonData = [];
 let offset = 0;
@@ -17,39 +15,6 @@ window.addEventListener('scroll', async () => {
         renderPokemonGallery(nextPokemonChunk);
     }
 });
-
-searchButton.addEventListener('click', async () => {
-    const userInput = searchInput.value.toLowerCase().trim();
-
-    const pokemon = allPokemonData.find(pokemon =>
-        pokemon.name === userInput
-    )
-
-    if (!pokemon) {
-        error.classList.remove('opacity-0', 'scale-90', 'hidden');
-        error.classList.add('opacity-100', 'scale-100');
-    } else {
-        error.classList.remove('opacity-100', 'scale-100');
-        error.classList.add('opacity-0', 'scale-90', 'hidden');
-
-        const { name, type, image, id } = await getPokemonDetails(pokemon.url);
-
-        pokemonGallery.innerHTML = '';
-
-        const pokemonCard = document.createElement('div');
-        pokemonCard.className = 'flex flex-col items-center justify-center p-2 border border-utOrange rounded-lg shadow-lg shadow-utOrange bg-prussianBlue text-skyBlue';
-        pokemonCard.innerHTML = `
-            <p class='text-lg font-semibold self-end'>${id}</p>
-            <a href='/src/pages/pokemon-details.html?id=${id}' class='hover:-translate-y-1 hover:scale-105 transition-all ease-in'>
-            <img src=${image} alt=${name} class="w-[10rem] h-auto" />
-            </a>
-            <p key=${id} class='text-lg font-semibold capitalize'>${name}</p>
-            <p class='capitalize italic'>${type}</p>
-        `;
-
-        pokemonGallery.appendChild(pokemonCard);
-    }
-})
 
 searchInput.addEventListener('input', async (event) => {
     const userInput = searchInput.value.toLowerCase().trim();
